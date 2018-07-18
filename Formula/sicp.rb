35f181edac0e790079bfc3d67b99867a10d0e15a
Shawn Shah <shawnshah@Shawns-MBP.charter.com>
2018-07-17 18:18:33 -0700

    fix sicp sha256

--- Formula/sicp.rb
+++ Formula/sicp.rb
@@ -2,7 +2,7 @@ class Sicp < Formula
   desc "SICP Support for DrRacket"
   homepage "http://www.neilvandyke.org/racket/sicp/"
   url "https://github.com/sicp-lang/sicp/archive/0add3ff174d519abfde4bccf09f4664387c95763.zip"
-  sha256 "7f85e55f2c6eb8fe53eeb7432baba3e54269786984db55f5c0fad57604b1d2d7"
+  sha256 "20979241c861f7a5ecb04eb54a03d999c59b9321bc044e005ac68f02a2745ca9"
   revision 1
 
   bottle :unneeded
